3e90aaa5356300f50a25083eab145083b1c07b30
Brenda Kiptim <kiptim54@gmail.com>
2018-03-15 09:44:26 +0300

    add initital setup

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {Goal } from './goal'
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+
+export class AppComponent {
+  constructor(){}
+}
